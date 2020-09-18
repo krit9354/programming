e1806f161b6b9b52afc68140b15354e13b2aae39
krit9354 <krit9354@gmail.com>
2020-09-19 01:36:27 +0700

    dont know?
    dictionary full version

--- test.py
+++ test.py
@@ -1,2 +1,7 @@
-import autopy
-autopy.mouse.move(1,1)
+import csv
+
+with open('employee_file.csv', mode='w',encoding='utf-8') as employee_file:
+    employee_writer = csv.writer(employee_file, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
+
+    employee_writer.writerow(['John Smith', 'Accounting', 'November'])
+    employee_writer.writerow(['Erica Meyers', 'IT', 'March'])
